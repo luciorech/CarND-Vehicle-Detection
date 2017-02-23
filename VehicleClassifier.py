c7913b4f630b98dbd0f81df53266975dbcfe1aa2
luciorech <luciorech@gmail.com>
2017-02-22 20:27:50 -0800

    Creating classifier class, slightly more organizes

---
+++ VehicleClassifier.py
@@ -0,0 +1,159 @@
+import matplotlib.image as mpimg
+import numpy as np
+import cv2
+import glob
+import time
+from skimage.feature import hog
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+
+class VehicleClassifier:
+
+    def __init__(self,
+                 color_space='RGB',
+                 spatial_size=(32, 32),
+                 color_histogram_bins=32,
+                 orientations=9,
+                 pixels_per_cell=8,
+                 cells_per_block=2,
+                 hog_channels=[0, 1, 2],
+                 spatial_features=True,
+                 color_histogram_features=True,
+                 hog_features=True):
+        self._color_space = color_space
+        self._spatial_size = spatial_size
+        self._color_histogram_bins = color_histogram_bins
+        self._orientations = orientations
+        self._pixels_per_cell = pixels_per_cell
+        self._cells_per_block = cells_per_block
+        self._hog_channels = hog_channels
+        self._spatial_features = spatial_features
+        self._color_histogram_features = color_histogram_features
+        self._hog_features = hog_features
+        self._classifier = None
+        self._scaler = None
+
+    def train(self,
+              vehicles_path,
+              non_vehicles_path,
+              sample_size=None):
+
+        cars = glob.glob(vehicles_path)
+        not_cars = glob.glob(non_vehicles_path)
+
+        if sample_size is None:
+            sample_size = max(len(cars), len(not_cars))
+
+        cars = cars[0:sample_size]
+        not_cars = not_cars[0:sample_size]
+
+        features = []
+        for file in cars + not_cars:
+            hist_range = (0, 256)
+            if file.lower().endswith("png"):
+                hist_range = (0., 1.)
+            # todo: test if png or jpg, set bin range accordingly
+            image = mpimg.imread(file)
+            features.append(self._extract_features(image, hist_range))
+        print('Feature vector length:', len(features[0]))
+
+        x = np.array(features).astype(np.float64)
+        self._scaler = StandardScaler().fit(x)
+        scaled_x = self._scaler.transform(x)
+        y = np.concatenate([np.ones(len(cars)), np.zeros(len(not_cars))])
+
+        x_train, x_test, y_train, y_test = \
+            train_test_split(scaled_x, y, test_size=0.2, random_state=27)
+
+        self._classifier = LinearSVC()
+        t = time.time()
+        self._classifier.fit(x_train, y_train)
+        t2 = time.time()
+        print(round(t2 - t, 2), 'Seconds to train classifier...')
+        print('Test Accuracy of classifier = ', round(self._classifier.score(x_test, y_test), 4))
+
+    def _extract_features(self,
+                          image,
+                          histogram_range):
+        features = []
+        # todo: improve conversion, deal with BGR inputs
+        # todo: do we really need to copy the image?
+        if self._color_space != 'RGB':
+            if self._color_space == 'HSV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+            elif self._color_space == 'LUV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2LUV)
+            elif self._color_space == 'HLS':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+            elif self._color_space == 'YUV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+            elif self._color_space == 'YCrCb':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YCrCb)
+        else:
+            feature_image = np.copy(image)
+
+        if self._spatial_features:
+            feat = self._bin_spatial(feature_image)
+            features.append(feat)
+
+        if self._color_histogram_features:
+            feat = self._color_hist(feature_image, bins_range=histogram_range)
+            features.append(feat)
+
+        if self._hog_features:
+            hog_features = []
+            for channel in self._hog_channels:
+                hog_features.append(self._get_hog_features(feature_image[:, :, channel],
+                                                           visualize=False,
+                                                           feature_vector=True))
+            hog_features = np.ravel(hog_features)
+            features.append(hog_features)
+
+        return np.concatenate(features)
+
+    def _get_hog_features(self,
+                          img,
+                          visualize=False,
+                          feature_vector=True):
+        """
+        Extracts HOG features from a channel
+        If visualize is True, also returns an image representing the extracted HOG features
+        """
+        if visualize:
+            features, hog_image = hog(img,
+                                      orientations=self._orientations,
+                                      pixels_per_cell=(self._pixels_per_cell, self._pixels_per_cell),
+                                      cells_per_block=(self._cells_per_block, self._cells_per_block),
+                                      transform_sqrt=True,
+                                      visualise=visualize,
+                                      feature_vector=feature_vector)
+            return features, hog_image
+        else:
+            features = hog(img,
+                           orientations=self._orientations,
+                           pixels_per_cell=(self._pixels_per_cell, self._pixels_per_cell),
+                           cells_per_block=(self._cells_per_block, self._cells_per_block),
+                           transform_sqrt=True,
+                           visualise=visualize,
+                           feature_vector=feature_vector)
+            return features
+
+    def _bin_spatial(self, img):
+        """
+        Resizes an image and flattens the result in a 1-D array
+        """
+        features = cv2.resize(img, self._spatial_size).ravel()
+        return features
+
+    def _color_hist(self, img, bins_range=(0, 256)):
+        """
+        Computes the histogram for each image channel and concatenates it in a single array
+        """
+        # todo: Expects a 3-channel image, make this generic
+        channel1_hist = np.histogram(img[:, :, 0], bins=self._color_histogram_bins, range=bins_range)
+        channel2_hist = np.histogram(img[:, :, 1], bins=self._color_histogram_bins, range=bins_range)
+        channel3_hist = np.histogram(img[:, :, 2], bins=self._color_histogram_bins, range=bins_range)
+        hist_features = np.concatenate((channel1_hist[0], channel2_hist[0], channel3_hist[0]))
+        return hist_features
--- train_classifier.py
+++ train_classifier.py
@@ -10,17 +10,11 @@ import time
 import pickle
 import matplotlib.image as mpimg
 
+from VehicleClassifier import VehicleClassifier
+
 if __name__ == "__main__":
     vehicles_path = './datasets/vehicles/GTI_Far/*.png'
     non_vehicles_path = './datasets/non-vehicles/GTI/*.png'
-
-    cars = glob.glob(vehicles_path)
-    not_cars = glob.glob(non_vehicles_path)
-
-    sample_size = 500
-
-    cars = cars[0:sample_size]
-    not_cars = not_cars[0:sample_size]
 
     color_space = 'YUV'
     spatial_size = (32, 32)
@@ -29,58 +23,21 @@ if __name__ == "__main__":
     pix_per_cell = 8
     cell_per_block = 2
     hog_channel = "ALL"
-    spatial_feat = True
-    hist_feat = True
+    spatial_feat = False
+    hist_feat = False
     hog_feat = True
 
-    car_features = ip.extract_features_files(cars,
-                                       color_space=color_space,
-                                       spatial_size=spatial_size,
-                                       hist_bins=hist_bins,
-                                       orient=orient,
-                                       pix_per_cell=pix_per_cell,
-                                       cell_per_block=cell_per_block,
-                                       hog_channel=hog_channel,
-                                       spatial_feat=spatial_feat,
-                                       hist_feat=hist_feat,
-                                       hog_feat=hog_feat)
-    not_car_features = ip.extract_features_files(not_cars,
-                                           color_space=color_space,
-                                           spatial_size=spatial_size,
-                                           hist_bins=hist_bins,
-                                           orient=orient,
-                                           pix_per_cell=pix_per_cell,
-                                           cell_per_block=cell_per_block,
-                                           hog_channel=hog_channel,
-                                           spatial_feat=spatial_feat,
-                                           hist_feat=hist_feat,
-                                           hog_feat=hog_feat)
-
-    X = np.vstack((car_features, not_car_features)).astype(np.float64)
-
-    # Fit a per-column scaler
-    X_scaler = StandardScaler().fit(X)
-    # Apply the scaler to X
-    scaled_X = X_scaler.transform(X)
-
-    # Define the labels vector
-    y = np.hstack((np.ones(len(car_features)), np.zeros(len(not_car_features))))
-
-    # Split up data into randomized training and test sets
-    rand_state = np.random.randint(0, 100)
-    X_train, X_test, y_train, y_test = train_test_split(
-        scaled_X, y, test_size=0.2, random_state=rand_state)
-
-    print('Feature vector length:', len(X_train[0]))
-    # Use a linear SVC
-    svc = LinearSVC()
-    # Check the training time for the SVC
-    t = time.time()
-    svc.fit(X_train, y_train)
-    t2 = time.time()
-    print(round(t2 - t, 2), 'Seconds to train SVC...')
-    # Check the score of the SVC
-    print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+    v_classifier = VehicleClassifier(color_space=color_space,
+                                     spatial_size=spatial_size,
+                                     color_histogram_bins=hist_bins,
+                                     orientations=orient,
+                                     pixels_per_cell=pix_per_cell,
+                                     cells_per_block=cell_per_block,
+                                     hog_channels=[0, 1, 2],
+                                     spatial_features=spatial_feat,
+                                     color_histogram_features=hist_feat,
+                                     hog_features=hog_feat)
+    v_classifier.train(vehicles_path, non_vehicles_path, 500)
 
     # Check the prediction time for a single sample
     t = time.time()
@@ -96,7 +53,7 @@ if __name__ == "__main__":
     windows = ip.slide_window(image, x_start_stop=[None, None], y_start_stop=[400, None],
                               xy_window=(96, 96), xy_overlap=(0.5, 0.5))
 
-    hot_windows = ip.search_windows(image, windows, svc, X_scaler,
+    hot_windows = ip.search_windows(image, windows, v_classifier._classifier, v_classifier._scaler,
                                     color_space=color_space,
                                     spatial_size=spatial_size,
                                     hist_bins=hist_bins,
